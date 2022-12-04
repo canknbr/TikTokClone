147983a3659e19c93940f9112b153d52cf9ff1e7
Can Kanbur <cankanbur1@gmail.com>
2022-12-04 19:13:38 +0300

    added folder structure

---
+++ src/components/Feed/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import Post from '../Post';
+const Feed = () => {
+  return (
+    <View>
+      <Text>Feed</Text>
+    </View>
+  );
+};
+
+export default Feed;
---
+++ src/components/Post/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import styles from './styles';
+const Post = () => {
+  return (
+    <View>
+      <Text>Post</Text>
+    </View>
+  );
+};
+
+export default Post;
